f26d558ac99bf58e41d5f7534d53476f68b8bb53
AlexeySomeRandomNumbersToRegister <141126246+AlexeySomeRandomNumbersToRegister@users.noreply.github.com>
2024-01-19 14:16:41 +0300

    Update cat_3.c

--- cat-grep/src/cat/cat_3.c
+++ cat-grep/src/cat/cat_3.c
@@ -3,24 +3,32 @@
 #include <string.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 4096
-
-void processFile(FILE *file, int bflag, int eflag, int nflag, int sflag);
+void processFile(FILE *file, int bflag, int eflag, int nflag, int sflag, int tflag);
+void processSFlag(char *buffer, int *lastLineBlank, int sflag);
+void processBFlag(char *buffer, int bflag, int *lineNumber);
+void processNFlag(char *buffer, int nflag, int *lineNumber);
+void processEFlag(char *buffer, int eflag);
+void processTFlag(char *buffer, int tflag);
 
 int main(int argc, char *argv[]) {
-    int bflag = 0, eflag = 0, nflag = 0, sflag = 0;
+    int bflag = 0, eflag = 0, nflag = 0, sflag = 0, tflag = 0;
     int flags;
 
-    while ((flags = getopt(argc, argv, "bens")) != -1) {
+    while ((flags = getopt(argc, argv, "beEnstT")) != -1) {
         switch(flags) {
             case 'b':
                 bflag++;
                 break;
             case 'e':
+            case 'E':
                 eflag++;
                 break;
             case 'n':
                 nflag++;
+                break;
+            case 't':
+            case 'T':
+                tflag++;
                 break;
             case 's':
                 sflag++;
@@ -28,17 +36,16 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    int currentFileIndex = optind; // Индекс аргумента, который является файлом. Отрабатывает, когда флаги заканчиваются или их не было найдено
+    int currentFileIndex = optind;
 
-    while (currentFileIndex < argc) { // Этот цикл необходим для обработки нескольких файлов последовательно
-        FILE *file = fopen(argv[currentFileIndex], "rb");
+    while (currentFileIndex < argc) {
+        FILE *file = fopen(argv[currentFileIndex], "r");
         if (file == NULL) {
-            fprintf(stderr, "%s: %s: No such file or directory\n", argv[0], argv[currentFileIndex]); // Выводится название исполняемого файла, 
-            // название текущего файла, который не был найдет и текст ошибки.
+            fprintf(stderr, "%s: %s: No such file or directory\n", argv[0], argv[currentFileIndex]);
             exit(1);
         }
 
-        processFile(file, bflag, eflag, nflag, sflag);
+        processFile(file, bflag, eflag, nflag, sflag, tflag);
         fclose(file);
 
         currentFileIndex++;
@@ -47,50 +54,86 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-void processFile(FILE *file, int bflag, int eflag, int nflag, int sflag) {
-    const int bufferSize = BUFFER_SIZE; // Надо подумать как избавиться от этого
-    char buffer[bufferSize];
+void processFile(FILE *file, int bflag, int eflag, int nflag, int sflag, int tflag) {
+    char *buffer = NULL;
+    size_t bufferSize = 0;
     int lastLineBlank = 0;
     int lineNumber = 1;
 
-    while (fgets(buffer, bufferSize, (file == NULL ? stdin : file))) { // Стандартный ввод нам необязательно обрабатывать, поэтому можно будет убрать
-        int length = strlen(buffer);
-        buffer[length - 1] = '\0';
+    while ((getline(&buffer, &bufferSize, file)) != -1) {
+        processSFlag(buffer, &lastLineBlank, sflag);
+        processBFlag(buffer, bflag, &lineNumber);
+        processNFlag(buffer, nflag, &lineNumber);
+        processEFlag(buffer, eflag);
+        processTFlag(buffer, tflag);
 
-        if (sflag) {
-            int currentLineBlank = (length <= 1) ? 1 : 0;
-            if (lastLineBlank && currentLineBlank) {
-                continue;
-            }
-            lastLineBlank = currentLineBlank;
-        }
+        fprintf(stdout, "%s", buffer);
+    }
 
-        if (bflag) {
-            if (length >= 1) {
-                char *tmp = strdup(buffer);
-                buffer[0] = '\0';
-                sprintf(buffer, "%*d\t", 6, lineNumber++);
-                strcat(buffer, tmp);
-            }
-        } else if (nflag) {
-            char *tmp = strdup(buffer);
+    free(buffer);
+}
+
+void processSFlag(char *buffer, int *lastLineBlank, int sflag) {
+    int length = strlen(buffer);
+    if (sflag) {
+        int currentLineBlank = (length <= 1) ? 1 : 0;
+        if (*lastLineBlank && currentLineBlank) {
             buffer[0] = '\0';
-            sprintf(buffer, "%*d\t", 6, lineNumber++);
-            strcat(buffer, tmp);
         }
-
-        if (eflag) {
-            length = strlen(buffer);
-            buffer[length] = '$';
-            buffer[length + 1] = '\0';
-        }
-
-        fprintf(stdout, "%s\n", buffer);
+        *lastLineBlank = currentLineBlank;
     }
 }
 
+void processBFlag(char *buffer, int bflag, int *lineNumber) {
+    int length = strlen(buffer);
+    if (bflag && length > 1) {
+        char *tmp = strdup(buffer);
+        buffer[0] = '\0';
+        sprintf(buffer, "%*d  ", 6, (*lineNumber)++);
+        strcat(buffer, tmp);
+        free(tmp);
+    }
+}
 
-// Добавить: E, t, T, A, v, флаг b реализован некорректно
+void processNFlag(char *buffer, int nflag, int *lineNumber) {
+    if (nflag) {
+        char *tmp = strdup(buffer);
+        buffer[0] = '\0';
+        sprintf(buffer, "%*d  ", 6, (*lineNumber)++);
+        strcat(buffer, tmp);
+        free(tmp);
+    }
+}
+
+void processEFlag(char *buffer, int eflag) {
+    int length = strlen(buffer);
+    if (eflag) {
+        if (length > 0 && buffer[length - 1] == '\n') {
+            buffer[length - 1] = '\0';
+            sprintf(buffer + length - 1, "%s", "$\n");
+        } else {
+            sprintf(buffer + length, "%s", "$");
+        }
+    }
+}
+
+void processTFlag(char *buffer, int tflag) {
+    int length = strlen(buffer);
+    if (tflag) {
+        for (int i = 0; i < length; i++) {
+            if (buffer[i] == '\t') {
+                memmove(buffer + i + 2, buffer + i + 1, length - i);
+                buffer[i] = '^';
+                buffer[i + 1] = 'I';
+                length++;
+                i++;
+            }
+        }
+    }
+}
+
+// Добавить: A
+// Флаги -t с -n работают некорректно вместе: 36      37      38      39      40  Объяснения от самого линукса:
 // Сделать обработку краевых ситуаций по типу неизвестного флага
 // Придумать как в case запихнуть длинные названия флагов
 // Устранить ошибку, что после использования неизвестного флага, файл все равно открывается
